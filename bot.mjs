f268d50af22e1a38d65b2db35724858317a0c7ad
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-03-12 02:38:38 +0500

    Prototype

--- bot.mjs
+++ bot.mjs
@@ -1,7 +1,10 @@
 import TeleBot from "telebot"
+import {md} from "telegram-md"
 
 const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
 
-bot.on('text', msg => msg.reply.text(msg.text))
+const options = {parseMode: "MarkdownV2", asReply: true}
+
+bot.on('*', msg => msg.reply.text(md.build(md.codeBlock(msg, "json")), options))
 
 export default bot
