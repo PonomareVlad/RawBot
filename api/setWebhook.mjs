4c90e358acb47925f97691bca8e134300171a182
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-04-05 04:11:10 +0500

    Added command for emoji

--- api/setWebhook.mjs
+++ api/setWebhook.mjs
@@ -1,4 +1,7 @@
-import {setWebhook} from 'telebot-vercel'
-import bot from '../bot.mjs'
+import commands from "../commands.json" assert {type: "json"}
+import {setWebhook} from "telebot-vercel"
+import bot from "../bot.mjs"
+
+await bot.setMyCommands(commands).catch(console.error)
 
 export default setWebhook({bot, path: 'api/telegram.mjs'})
--- bot.mjs
+++ bot.mjs
@@ -1,10 +1,25 @@
 import TeleBot from "telebot"
 import {md} from "telegram-md"
+import {NewMethodsMixin, parseCommands} from "telebot-utils"
 
-const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
+const TeleBotWithNewMethods = NewMethodsMixin(TeleBot)
+
+const bot = new TeleBotWithNewMethods(process.env.TELEGRAM_BOT_TOKEN)
 
 const options = {parseMode: "MarkdownV2", asReply: true}
 
-bot.on('*', msg => msg.reply.text(md.build(md.codeBlock(JSON.stringify(msg, null, 2), "json")), options))
+bot.mod("message", parseCommands)
+
+bot.on("*", msg => msg.reply.text(md.build(md.codeBlock(JSON.stringify(msg, null, 2), "json")), options))
+
+bot.on("text", async ({isCommand, command, entities = [], reply = {}} = {}) => {
+    if (!isCommand) return
+    switch (command) {
+        case "emoji":
+            const custom_emoji_ids = entities.map(({custom_emoji_id} = {}) => custom_emoji_id).filter(Boolean)
+            const result = await bot.getCustomEmojiStickers({custom_emoji_ids})
+            return reply.text(md.build(md.codeBlock(JSON.stringify(result, null, 2), "json")), options)
+    }
+})
 
 export default bot
